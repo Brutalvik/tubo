0bfa4f208b16360b1e7010480957d31a3cbee437
Vikram Kumar <vikcanada90@gmail.com>
2025-01-16 16:33:43 +0700

    Added nextui and store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { Provider } from "react-redux";
+import store from "./store/store";
 import App from "./App";
+import "./index.css";
 
 const node = document.getElementById("root");
 const root = createRoot(node);
 const app = (
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <NextUIProvider>
+        <main className="dark text-foreground bg-background">
+          <App />
+        </main>
+      </NextUIProvider>
+    </Provider>
   </StrictMode>
 );
 root.render(app);
