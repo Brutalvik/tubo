2344de308dfe79ecf7a457f93ecc7e6df5e07e77
Vikram Kumar <vikcanada90@gmail.com>
2025-01-16 23:50:25 +0700

    updated calender date range change handling and styling

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,19 +1,33 @@
 import { Button, DateRangePicker } from "@nextui-org/react";
 import { parseZonedDateTime } from "@internationalized/date";
 import { Input } from "@nextui-org/react";
-import {
-  GoogleMap,
-  useJsApiLoader,
-  StandaloneSearchBox,
-} from "@react-google-maps/api";
+import { useJsApiLoader, StandaloneSearchBox } from "@react-google-maps/api";
 import { useRef } from "react";
+import moment from "moment-timezone";
+import { useState } from "react";
 
 const Calendar = () => {
+  const today =
+    moment().tz("America/Los_Angeles").format("YYYY-MM-DDTHH:mm:ss") +
+    `[America/Los_Angeles]`;
+  const plusThreeDays =
+    moment()
+      .tz("America/Los_Angeles")
+      .add(3, "days") // Adds 3 days
+      .format("YYYY-MM-DDTHH:mm:ss") + `[America/Los_Angeles]`;
+
+  const [dateRange, setDateRange] = useState({
+    start: parseZonedDateTime(today),
+    end: parseZonedDateTime(plusThreeDays),
+  });
+  const [isCalenderOpen, setIsCalendarOpen] = useState(false);
+
+  const libraries = ["places"];
   const inputRef = useRef();
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
-    libraries: ["places"],
+    libraries: libraries,
     googleMapsClientId: process.env.REACT_APP_CLIENT_ID,
   });
 
@@ -22,6 +36,27 @@ const Calendar = () => {
   const handleOnPlacesChanged = () => {
     let address = inputRef.current.getPlaces();
     console.log(address);
+  };
+
+  const handleDateChange = (range) => {
+    const formattedStart =
+      moment
+        .tz(range.start, "America/Los_Angeles")
+        .format("YYYY-MM-DDTHH:mm:ss") + "[America/Los_Angeles]";
+
+    const formattedEnd =
+      moment
+        .tz(range.end, "America/Los_Angeles")
+        .format("YYYY-MM-DDTHH:mm:ss") + "[America/Los_Angeles]";
+
+    setDateRange({
+      start: parseZonedDateTime(formattedStart),
+      end: parseZonedDateTime(formattedEnd),
+    });
+
+    console.log("Formatted Start Date: ", formattedStart);
+    console.log("Formatted End Date: ", formattedEnd);
+    setIsCalendarOpen(false);
   };
 
   return (
@@ -33,7 +68,13 @@ const Calendar = () => {
             onLoad={(ref) => (inputRef.current = ref)}
             onPlacesChanged={handleOnPlacesChanged}
           >
-            <Input key="inside" label="Where" type="text" className="w-full" />
+            <Input
+              key="inside"
+              label="Where"
+              type="text"
+              className="w-full"
+              placeholder=""
+            />
           </StandaloneSearchBox>
         )}
       </div>
@@ -41,13 +82,23 @@ const Calendar = () => {
       {/* Date range picker */}
       <div className="flex w-full gap-4">
         <DateRangePicker
+          isOpen={isCalenderOpen}
+          onOpenChange={() =>
+            setIsCalendarOpen((isCalenderOpen) => !isCalenderOpen)
+          }
           hideTimeZone
+          onChange={handleDateChange}
           defaultValue={{
-            start: parseZonedDateTime("2024-04-01T00:45[America/Los_Angeles]"),
-            end: parseZonedDateTime("2024-04-08T11:15[America/Los_Angeles]"),
+            start: dateRange.start,
+            end: dateRange.end,
           }}
-          label="Event duration"
+          value={{
+            start: dateRange.start,
+            end: dateRange.end,
+          }}
+          label="Booking Dates"
           visibleMonths={2}
+          minValue={parseZonedDateTime(today)}
         />
         <Button className="h-[15px]" variant="solid">
           Search
