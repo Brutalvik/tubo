b97441f5a40bd5b02e8fd4ba3917eac2c44de728
Vikram Kumar <vikcanada90@gmail.com>
2025-01-16 22:07:17 +0700

    Added google places api to the app

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,13 +1,41 @@
 import { Button, DateRangePicker } from "@nextui-org/react";
 import { parseZonedDateTime } from "@internationalized/date";
 import { Input } from "@nextui-org/react";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  StandaloneSearchBox,
+} from "@react-google-maps/api";
+import { useRef } from "react";
 
 const Calendar = () => {
+  const inputRef = useRef();
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_MAPS_API_KEY,
+    libraries: ["places"],
+    googleMapsClientId: process.env.REACT_APP_CLIENT_ID,
+  });
+
+  console.log(isLoaded);
+
+  const handleOnPlacesChanged = () => {
+    let address = inputRef.current.getPlaces();
+    console.log(address);
+  };
+
   return (
     <div className="w-[300px] max-w-xl flex flex-col gap-2 ">
       {/* Input for location */}
       <div className="w-full">
-        <Input key="inside" label="Where" type="text" className="w-full" />
+        {isLoaded && (
+          <StandaloneSearchBox
+            onLoad={(ref) => (inputRef.current = ref)}
+            onPlacesChanged={handleOnPlacesChanged}
+          >
+            <Input key="inside" label="Where" type="text" className="w-full" />
+          </StandaloneSearchBox>
+        )}
       </div>
 
       {/* Date range picker */}
