08bb9ee728d7cf50a6ada6e7ddc48f530b15345f
Vikram Kumar <vikcanada90@gmail.com>
2025-01-16 18:46:13 +0700

    added banner and fixed banner styling

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import bannerImage from "@assets/banner.jpg"; // Adjust the path as necessary
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <div className="w-[728] h-[90] bg-cover bg-center mt-2">
+      <img src={bannerImage} alt="banner" />
+    </div>
+  );
 };
 
 export default Banner;
