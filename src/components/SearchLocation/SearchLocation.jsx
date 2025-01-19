f8a76916f10eac8cae7bc19753e8466f553a4af9
Vikram Kumar <vikcanada90@gmail.com>
2025-01-19 11:38:41 -0700

    Reapply "fixed card blurriness"
    This reverts commit 0460f8940321b34431c7029fe21ace321b62465f.

--- src/components/SearchLocation/SearchLocation.jsx
+++ src/components/SearchLocation/SearchLocation.jsx
@@ -1,20 +1,10 @@
 import React from "react";
-import { Input } from "@heroui/react";
+import Calendar from "@components/Calendar/Calendar";
 
 const SearchLocation = () => {
   return (
     <div className="flex items-center gap-4 mt-6">
-      {/* Editable Input Box */}
-      <p>Where</p>
-      <Input
-        type="text"
-        placeholder="Search location"
-        className="border border-gray-300 rounded-md px-4 py-2"
-      />
-
-      {/* Date/Time Range */}
-      <Input type="datetime-local" className=" rounded-md px-4 py-2" />
-      <Input type="datetime-local" className="rounded-md px-4 py-2" />
+      <Calendar />
     </div>
   );
 };
