91b9af0c58be8e8d1c9d39afcb93c4694d8a9e09
Vikram Kumar <vikcanada90@gmail.com>
2025-01-20 07:06:31 -0700

    updated structure

--- src/pages/Results/Results.jsx
+++ src/pages/Results/Results.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import CarCard from "@features/CarCard/CarCard";
+import cars from "../../dummy/cars";
 
 const Results = () => {
+  console.log(cars);
   return <CarCard />;
 };
 
